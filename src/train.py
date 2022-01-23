a7eb826ba43cf7061ce4c41e6ca393f20e7244c0
hadishh <hadsheikhi@gmail.com>
2022-01-23 10:54:30 +0330

    train model

--- src/train.py
+++ src/train.py
@@ -1,11 +1,14 @@
+from ast import mod
 import torch
 from torch import nn
 from tqdm import tqdm
 from src.crnn import CRNN
 from src.national_id_dataset import NationalIdDataset
 
-TOTAL_BATCHES = 20
+TOTAL_BATCHES = 50000
 BATCH_SIZE = 32
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def train_batch(model, criterion, optimizer, batch, target, target_lengths):
@@ -19,18 +22,32 @@ def train_batch(model, criterion, optimizer, batch, target, target_lengths):
 
 
 if __name__ == '__main__':
-    model = CRNN()
+    model = CRNN().to(device)
     optimizer = torch.optim.Adam(model.parameters())
     criterion = nn.CTCLoss(blank=0, zero_infinity=True, reduction='mean')
-
 
     dataset = NationalIdDataset(batch_size=BATCH_SIZE)
     datasetIterator = iter(dataset)
 
-
+    i = 0
+    history = []
     for _ in tqdm(range(TOTAL_BATCHES)):
         x, y = next(datasetIterator)
+        if i % 1000 == 1:
+            torch.save({
+                "model": model.state_dict(),
+                "Iteration": i,
+                "Loss": cost,
+                "Optimizer": optimizer.state_dict(),
+                "History": history
+            }, f"checkpoint/{i}.torch")
+
+        x = x.to(device)
+        y = y.to(device)
+
         y_lengths = torch.full(
-            size=(y.shape[0],), fill_value=y.shape[1], dtype=torch.int32
-        )
+            size=(y.shape[0],), fill_value=y.shape[1], dtype=torch.int32).to(device)
+
         cost = train_batch(model, criterion, optimizer, x, y, y_lengths)
+        history.append(cost.cpu().detach().numpy())
+        i += 1
